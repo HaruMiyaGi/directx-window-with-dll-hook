9f6d294ee1cc52b9a9c9425879c0346b92cdecfb
HaruMiyaGi <66522128+HaruMiyaGi@users.noreply.github.com>
2020-09-26 18:55:54 +0100

    Fixed project debug startup

--- Hook/Main.cpp
+++ Hook/Main.cpp
@@ -1,8 +1,24 @@
 #include <Windows.h>
+#include <d3d11.h>
+
+#pragma comment (lib, "d3d11.lib")
+
+
+
+ID3D11Device* pDevice = nullptr;
+IDXGISwapChain* pSwapChain = nullptr;
+ID3D11DeviceContext* pContex = nullptr;
+
+
+
+bool HookD3D();
 
 void MainThread(void* pHandle)
 {
-	MessageBoxA(nullptr, "Hooked!", "YAY!!", MB_OK);
+	if (HookD3D())
+		MessageBoxA(nullptr, "Hooked!", "YAY!!", MB_OK);
+	else
+		MessageBoxA(nullptr, "FAILED!", "SADGE!!", MB_OK);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
@@ -17,4 +33,34 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 		break;
 	}
 	return TRUE;
+}
+
+bool HookD3D()
+{
+	DXGI_SWAP_CHAIN_DESC scd = { 0 };
+	scd.BufferDesc.Width = 0;
+	scd.BufferDesc.Height = 0;
+	scd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Quality = 0;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.BufferCount = 1;
+	scd.OutputWindow = GetForegroundWindow();
+	scd.Windowed = TRUE;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.Flags = 0;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		nullptr, D3D_DRIVER_TYPE_HARDWARE,
+		nullptr, 0, nullptr, 0,
+		D3D11_SDK_VERSION,
+		&scd, &pSwapChain, &pDevice, nullptr, &pContex
+	);
+
+	if (FAILED(hr))
+		return false;
+
+	return true;
 }
